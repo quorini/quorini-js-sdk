2f9794283752a181be7cb12f99604410f47f4119
Ernst <ernstchristia@gmail.com>
2024-11-08 02:04:33 +0800

    renamed scope temporarily

--- packages/quorini-react/rollup.config.mjs
+++ packages/quorini-react/rollup.config.mjs
@@ -12,5 +12,5 @@ export default {
       tsconfig: './tsconfig.json'
     })
   ],
-  external: ['react', 'react-dom', '@quorini/qui-core']
+  external: ['react', 'react-dom', '@ernst1202/qui-core']
 };
