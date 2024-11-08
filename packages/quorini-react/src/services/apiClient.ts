21bd76d76e252cdc4da906fe9a979ce33a171ccb
Ernst <ernstchristia@gmail.com>
2024-11-08 21:04:11 +0800

    test endpoint

--- packages/quorini-react/src/services/apiClient.ts
+++ packages/quorini-react/src/services/apiClient.ts
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
 // Define base URLs using environment variables
-const QUORINI_API = process.env.REACT_APP_QUORINI_API;
-const QUORINI_AUTH_API = process.env.REACT_APP_QUORINI_AUTH_API;
-const CUSTOMER_API = process.env.REACT_APP_CUSTOMER_API;
-const CUSTOMER_AUTH_API = process.env.REACT_APP_CUSTOMER_AUTH_API;
+// const QUORINI_API = process.env.REACT_APP_QUORINI_API;
+// const QUORINI_AUTH_API = process.env.REACT_APP_QUORINI_AUTH_API;
+// const CUSTOMER_API = process.env.REACT_APP_CUSTOMER_API;
+// const CUSTOMER_AUTH_API = process.env.REACT_APP_CUSTOMER_AUTH_API;
+
+const QUORINI_API = "https://nq3o4t9ax0.execute-api.us-west-2.amazonaws.com/development"
+const QUORINI_AUTH_API = "https://vlpw2q6zt5.execute-api.us-west-2.amazonaws.com/development"
+const CUSTOMER_API = "https://h5ti6dtzyl.execute-api.us-west-2.amazonaws.com/development"
+const CUSTOMER_AUTH_API = "https://hth72i9z93.execute-api.us-west-2.amazonaws.com/development"
 
 if (!QUORINI_API || !QUORINI_AUTH_API || !CUSTOMER_API || !CUSTOMER_AUTH_API) {
   console.warn("Some API environment variables are not defined.");
@@ -21,6 +26,7 @@ const apiClient = axios.create({
 // Login function
 export const login = async (username: string, password: string) => {
   try {
+    console.log("login-QUORINI_AUTH_API", QUORINI_AUTH_API)
     const response = await apiClient.post(`${QUORINI_AUTH_API}/log-in`, {
       authOption: { username, password },
     });
@@ -34,6 +40,7 @@ export const login = async (username: string, password: string) => {
 // Signup function
 export const signup = async (username: string, password: string) => {
   try {
+    console.log("signup-QUORINI_AUTH_API", QUORINI_AUTH_API)
     const response = await apiClient.post(`${QUORINI_AUTH_API}/sign-up`, {
         authOption: { username, password },
     });
