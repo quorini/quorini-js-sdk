fa6f992e99b5b4823136b807cda454b35a57f5e3
Ernst <ernstchristia@gmail.com>
2024-11-08 17:30:40 +0800

    fixed declaration file issue

--- packages/quorini-react/rollup.config.mjs
+++ packages/quorini-react/rollup.config.mjs
@@ -1,16 +1,24 @@
 import typescript from 'rollup-plugin-typescript2';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    sourcemap: true,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist',
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json'
+      })
+    ],
+    external: ['react', 'react-dom', '@ernst1202/qui-core']
   },
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json'
-    })
-  ],
-  external: ['react', 'react-dom', '@ernst1202/qui-core']
-};
+  {
+    input: 'src/index.ts', // Path to your entry declaration file
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
