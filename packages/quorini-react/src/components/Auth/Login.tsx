59a57efa3e42cde2adec4888eaadd2da884df934
Ernst <ernstchristia@gmail.com>
2024-11-12 01:56:06 +0800

    improved Login component

--- packages/quorini-react/src/components/Auth/Login.tsx
+++ packages/quorini-react/src/components/Auth/Login.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import styled from "styled-components"
-import { Button, Input, Typography, Form } from "antd"
-import { KeyOutlined, LoadingOutlined, LockOutlined, UserOutlined } from "@ant-design/icons"
+import { Button, Input, Form, Alert, Flex, Checkbox } from "antd"
+import { LockOutlined, UserOutlined } from "@ant-design/icons"
 import { useAuth } from '../../hooks';
-import { Colors, Spaces } from '../styles';
 
 interface LoginProps {
   onLoginSuccess: () => void;
@@ -15,57 +14,31 @@ const Login: React.FC<LoginProps> = ({ onLoginSuccess, onSignupClick }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleLogin = () => {
+    setIsLoading(true);
     login(username, password)
       .then(() => {
+        setIsLoading(false);
         onLoginSuccess();
       })
       .catch((err) => {
+        setIsLoading(false);
         console.log("login-err", err);
         setError("Invalid username or password");
       });
   };
 
   return (
-    <div>
-      {/* <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={onSignupClick}>Sign up</button>
-      {error && <p style={{ color: 'red' }}>{error}</p>} */}
-      <Selector
-        onClick={() => { console.log("Selector-onClicked") }}
-      >
-        <SelectorOption selected={true}>
-          <UserOutlined /> Log in
-        </SelectorOption>
-        <SelectorOption selected={false}>
-          <KeyOutlined /> Sign up
-        </SelectorOption>
-      </Selector>
-
-      <Form
-        onFinish={() => { console.log("onFinish") }}
-        autoComplete="off"
-      >
+    <LoginWrapper>
+      <Form onFinish={handleLogin} autoComplete="off">
         <Form.Item name="username" rules={[{ required: true, message: "Please input your email address!" }]}>
           <Input
             prefix={<UserOutlined />}
             placeholder="Email address..."
             title="Email address..."
-            onChange={(e) => { console.log("onChange-email", e.target.value) }}
+            onChange={(e) => setUsername(e.target.value)}
             size="large"
             style={{ width: "300px" }}
             autoFocus
@@ -90,62 +63,46 @@ const Login: React.FC<LoginProps> = ({ onLoginSuccess, onSignupClick }) => {
           ]}
         >
           <Input.Password
-            // disabled={state.matches("authPage.loggingIn") || state.matches("authPage.signingUp")}
             prefix={<LockOutlined />}
             placeholder="Password..."
             title="Password..."
-            onChange={(e) => { console.log("input.pass change", e.target.value) }}
+            onChange={(e) => setPassword(e.target.value)}
             size="large"
             style={{ width: "300px" }}
           />
         </Form.Item>
 
-        <div>
-          <Button
-            type="text"
-            onClick={() => {  }}
-          >
-            Forgot password?
-          </Button>
-          <Button
-            type="primary"
-            htmlType="submit"
-            // loading={state.matches("authPage.loggingIn") || state.matches("authPage.signingUp")}
-          >
+        {error && (
+          <Form.Item>
+            <Alert message={error} type="error" showIcon />
+          </Form.Item>
+        )}
+
+        <Form.Item>
+          <Flex justify="space-between" align="center">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <a href="">Forgot password</a>
+          </Flex>
+        </Form.Item>
+
+        <Form.Item>
+          <Button block type="primary" htmlType="submit" loading={isLoading}>
             Log in
           </Button>
-        </div>
+          or <a href="">Sign up now!</a>
+        </Form.Item>
       </Form>
-    </div>
+    </LoginWrapper>
   );
 };
 
-const Selector = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 0px;
-
-    border-radius: 20px;
-    border: 1px solid ${Colors.primary};
-    cursor: pointer;
-`;
-
-const SelectorOption = styled.div<{ selected: boolean }>`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 2px;
-
-    font-size: 14px;
-    padding: 3px ${Spaces.normal};
-    border-radius: 20px;
-    color: ${(props) => (props.selected ? Colors.background : Colors.primary)};
-    background: ${(props) => (props.selected ? Colors.primary : Colors.background)};
-
-    &:hover {
-        background: ${(props) => (props.selected ? Colors.primaryHover : Colors.grayLight)};
-    }
+const LoginWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  align-items: center;
 `;
 
 export default Login;
