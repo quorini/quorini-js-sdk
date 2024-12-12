53ad6ad392888924a4a3eef85b340b106c798b2b
Ernst <ernstchristia@gmail.com>
2024-12-13 02:19:58 +0800

    fixed gqlService bug

--- packages/quorini-core/src/services/gqlService.ts
+++ packages/quorini-core/src/services/gqlService.ts
@@ -1,28 +1,26 @@
-import { ApolloClient, InMemoryCache, HttpLink, gql, OperationVariables } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, gql, OperationVariables, NormalizedCacheObject } from '@apollo/client';
 import { QClient } from '../config';
 
 const SESSION_KEY = 'session';
-
-const session = JSON.parse(localStorage.getItem(SESSION_KEY)!);
-
-// Create an Apollo Client
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: `${QClient.getPrivate().apiUrl}/${QClient.getConfig().projectId}/gql${QClient.getConfig().env === 'development' ? `?env=dev` : ''}`,
-    headers: {
-      Authorization: `${session?.accessToken}`,
-    },
-  }),
-  cache: new InMemoryCache({
-    addTypename: false,
-  }),
-});
 
 export const query = async <VarsType extends OperationVariables, ResponseType>(
   baseQuery: string,
   variables?: VarsType,
   selectors?: string
 ): Promise<ResponseType> => {
+  const session = JSON.parse(localStorage.getItem(SESSION_KEY)!);
+
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: `${QClient.getPrivate().apiUrl}/${QClient.getConfig().projectId}/gql${QClient.getConfig().env === 'development' ? `?env=dev` : ''}`,
+      headers: {
+        Authorization: `${session?.accessToken}`,
+      },
+    }),
+    cache: new InMemoryCache({
+      addTypename: false,
+    }),
+  });
 
   // Ensure selectors are valid
   if (selectors && !/^[a-zA-Z0-9_ ]+$/.test(selectors)) {
@@ -60,6 +58,20 @@ export const mutate = async <VarsType extends OperationVariables, ResponseType>(
   baseMutation: string,
   variables: VarsType,
 ): Promise<ResponseType> => {
+  const session = JSON.parse(localStorage.getItem(SESSION_KEY)!);
+
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: `${QClient.getPrivate().apiUrl}/${QClient.getConfig().projectId}/gql${QClient.getConfig().env === 'development' ? `?env=dev` : ''}`,
+      headers: {
+        Authorization: `${session?.accessToken}`,
+      },
+    }),
+    cache: new InMemoryCache({
+      addTypename: false,
+    }),
+  });
+
   const mutation = gql(baseMutation);
 
   try {
