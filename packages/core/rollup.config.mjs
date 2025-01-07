05cedd4b893128efa68e8f99b220e8ae389d7cb4
Ernst <ernstchristia@gmail.com>
2025-01-08 00:07:09 +0800

    build cjs and ems

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -2,11 +2,16 @@ import typescript from 'rollup-plugin-typescript2';
 
 export default {
   input: './src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
   plugins: [
     typescript({
       tsconfig: './tsconfig.json'
