aea1dd0d1e4ad30a90d3e86e1f3f346c51402d6a
Ernst <ernstchristia@gmail.com>
2025-01-08 00:18:31 +0800

    update package.json and rollup.config in core package

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -1,21 +1,31 @@
 import typescript from 'rollup-plugin-typescript2';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-    },
-  ],
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json'
-    })
-  ],
-  external: ['@apollo/client']
-};
+export default [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json'
+      })
+    ],
+    external: []
+  },
+  {
+    input: './src/index.ts', // Path to your entry declaration file
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
