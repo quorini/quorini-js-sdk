4b23365180aae68760c316f7052ef94987699384
Ernst <ernstchristia@gmail.com>
2025-03-11 14:11:44 +0800

    add log to mutate

--- packages/core/src/services/gqlService.ts
+++ packages/core/src/services/gqlService.ts
@@ -169,6 +169,8 @@ export const mutate = async <VarsType extends OperationVariables, ResponseType>(
   const mutation = gql(baseMutation);
 
   try {
+    console.log("SDK-mutation", mutation)
+    console.log("SDK-variables", variables)
     const response = await client.mutate<ResponseType, VarsType>({
       mutation,
       variables,
