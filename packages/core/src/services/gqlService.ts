0bee0c67b9cfd68639309cb998253b373fc2b10b
Ernst <ernstchristia@gmail.com>
2025-05-20 22:10:48 +0800

    typename disable

--- packages/core/src/services/gqlService.ts
+++ packages/core/src/services/gqlService.ts
@@ -168,7 +168,7 @@ export const mutate = async <VarsType extends OperationVariables, ResponseType>(
       },
     }),
     cache: new InMemoryCache({
-      addTypename: true,
+      addTypename: false,
     }),
   });
 
